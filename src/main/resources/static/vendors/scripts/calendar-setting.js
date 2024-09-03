00164a7e7415343b7fad38e3cf2652b40393bddd
devkgn88 <devkgn88@gmail.com>
2024-09-03 14:49:06 +0900

    [kgn] Full Calendar 휴일 셋팅 동작 확인

--- src/main/resources/static/vendors/scripts/calendar-setting.js
+++ src/main/resources/static/vendors/scripts/calendar-setting.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', function() {
 	    },
       allDaySlot:false,
 	  slotMinTime:"09:00:00",
-	  slotMaxTime:"18:30:00",
+	  slotMaxTime:"18:00:00",
 	  slotLabelInterval:"00:30",
 	  locale:'ko',
 	  buttonText:{
@@ -20,6 +20,12 @@ document.addEventListener('DOMContentLoaded', function() {
 		day:'일간'
 	  },
 	  timeZone : 'local',
+	  businessHours: {
+	    daysOfWeek: [ 1, 2, 3, 4, 5 ], // Monday - Thursday
+
+	    startTime: '09:00', // a start time (10am in this example)
+	    endTime: '18:00', // an end time (6pm in this example)
+	  },
 	  eventDidMount: function(info) {
         var tooltip = new Tooltip(info.el, {
           title: info.event.extendedProps.description,
@@ -28,12 +34,49 @@ document.addEventListener('DOMContentLoaded', function() {
           container: 'body'
         });
       },
+	  
 	  events:[
 		{
-		  title: 'All Day Event',
-		  description: 'description for All Day Event',
-		  start: '2024-09-10'
-		}		
+			title: '[GDJ82] 1팀',
+	        start: '2024-09-03T11:00:00',
+	        end: '2024-09-03T11:30:00',
+	        description: '홍길동, 김민수, 이영희, 김철수, 홍길순',
+	        backgroundColor:'#F09EA7',
+	        borderColor:'#F09EA7',
+	        textColor:'#131e22'
+       },
+	   {
+		 title:'국군의 날',
+	     start: '2024-10-01',
+	     end: '2024-10-01',
+	     overlap: false,
+	     display: 'background',
+		 backgroundColor:'#ff00007d',
+	   },
+	   {
+   		 title:'추석',
+   	     start: '2024-09-15',
+   	     end: '2024-09-19',
+   	     overlap: false,
+   	     display: 'background',
+   		 backgroundColor:'#ff00007d',
+   	   },
+	   {
+  		 title:'개천절',
+  	     start: '2024-10-03',
+  	     end: '2024-10-03',
+  	     overlap: false,
+  	     display: 'background',
+  		 backgroundColor:'#ff00007d',
+  	   },
+	   {
+ 		 title:'한글날',
+ 	     start: '2024-10-09',
+ 	     end: '2024-10-09',
+ 	     overlap: false,
+ 	     display: 'background',
+ 		 backgroundColor:'#ff00007d',
+ 	   },
 	  ]
 	  
 	});
